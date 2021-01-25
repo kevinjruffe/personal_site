d709487c756b73498f4a9300944ab4aaf1254bca
Kevin Ruffe <kevinjruffe@gmail.com>
2021-01-25 00:01:20 -0500

    Added custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import H1 from "../components/H1";
+import PageLayout from "../components/PageLayout";
+
+export default function Custom404() {
+  return (
+    <PageLayout>
+      <div className="text-center">
+        <div className="text-green-light text-9xl pt-40 md:text-7xl sm:text-5xl">¯\_(ツ)_/¯</div>
+        <H1><span className="md:text-2xl">404 - Page Not Found</span></H1>
+      </div>
+    </PageLayout>
+  );
+}
