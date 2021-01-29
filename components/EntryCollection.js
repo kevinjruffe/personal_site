dcb9e6ca469de0460115cfe16c7420a63159694f
Kevin Ruffe <kevinjruffe@gmail.com>
2021-01-29 01:42:08 -0500

    pagination hook added

--- components/EntryCollection.js
+++ components/EntryCollection.js
@@ -1,14 +1,25 @@
 import Article from "../components/Article";
+import usePagination from "../hooks/usePagination";
 
 export default function EntryCollection({ heading, entries }) {
+  const { changePage, entriesPerPage, entriesToShow } = usePagination(entries);
+  const buttonStyle = "border border-solid border-green";
+
   return (
     <>
       <h1 className="pt-8 pb-1 text-green-light font-black text-base">
         {heading}
       </h1>
-      {entries.map((entry) => (
+      {entriesToShow.map((entry) => (
         <Article borderTop={true} entry={entry} key={entry.slug} />
       ))}
+      {entries.length > entriesPerPage && (
+        <>
+          <button className={buttonStyle}
+                  onClick={() => changePage(-1)}>Prior</button>
+          <button onClick={() => changePage()}>Next</button>
+        </>
+      )}
     </>
   );
 }
---
+++ hooks/usePagination.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function usePagination(entries) {
+  const [currentEntry, setCurrentEntry] = useState(0);
+  const entriesPerPage = 2;
+  const router = useRouter();
+  const totalPages = Math.ceil(entries.length / entriesPerPage);
+
+  const pathWithoutQuery = router.asPath.replace(/[?]page=-*[0-9]+/, "");
+
+  useEffect(() => {
+    if (router.query.page > 1 && router.query.page <= totalPages) {
+      setCurrentEntry(
+        Math.max(router.query.page - entriesPerPage, entriesPerPage)
+      );
+    } else if (router.query.page) {
+      router.push(pathWithoutQuery);
+    } else {
+      setCurrentEntry(0);
+    }
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
+
+  const entriesToShow = entries.slice(
+    currentEntry, currentEntry + entriesPerPage
+  );
+
+  const changePage = (change = 1) => {
+    const currentPage = Number(router.query.page ?? 1);
+    if (currentPage + change <= totalPages && currentPage + change >= 1) {
+      const page = Math.max(1, currentPage + change);
+      router.push(
+        page === 1 ? pathWithoutQuery : `${pathWithoutQuery}?page=${page}`
+      );
+    }
+  };
+
+  return { changePage, entriesPerPage, entriesToShow };
+}
