4af5d214adcddb388345f84beb95b71efa98feef
Kevin Ruffe <kevinjruffe@gmail.com>
2021-02-10 02:04:56 -0500

    list and link styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,11 +32,17 @@ module.exports = {
   plugins: [
     plugin(({ addBase, theme }) => {
       addBase({
-        h1: { fontSize: theme("fontSize.3xl") },
+        h1: { fontSize: theme("fontSize.3xl"), a: { color: "#215144" } },
         h2: { fontSize: theme("fontSize.2xl") },
         h3: { fontSize: theme("fontSize.xl") },
         p: { padding: "0.5rem 0" },
         ul: { padding: "0.5rem 0" },
+        li: {
+          listStyle: "circle",
+          listStylePosition: "inside",
+          padding: "0.25rem 0",
+        },
+        a: { color: "#7ca583", "&:hover": { textDecoration: "underline" } },
       });
     }),
   ],
